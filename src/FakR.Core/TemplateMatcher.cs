0d6b17e6b3a8537f82372207ead2f7720b129ac1
Jordan anderson <jordan_anders@live.co.uk>
2017-10-03 21:09:15 +0100

    Create place holders

---
+++ src/FakR.Core/TemplateMatcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FakR.Core
+{
+    public class TemplateMatcher
+    {
+        private readonly ITemplateStore _templateStore;
+
+        public TemplateMatcher(ITemplateStore templateStore)
+        {
+            _templateStore = templateStore;
+        }
+    }
+
+    public interface ITemplateStore
+    {
+        
+    }
+}
---
+++ tests/FakR.Tests/TemplateTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FakR.Core;
+using Moq;
+using NUnit.Framework;
+
+namespace FakR.Tests
+{
+    public class TemplateTests
+    {
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [Test]
+        public void GivenKnownTemplate_WhenMatchingStaticContent_ThenReturnsTemplate()
+        {
+            Mock<ITemplateStore> mockTemplateStore = new Mock<ITemplateStore>();
+
+            TemplateMatcher templateMatcher = new TemplateMatcher(mockTemplateStore.Object);
+
+        }
+    }
+}
