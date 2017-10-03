a8065b7934cd160475ec4a96423e7e05b34c32ed
Jordan Anderson <jordan_anders@live.co.uk>
2017-10-03 23:48:00 +0100

    Creates Hash of content when searching for template

--- src/FakR.Core/TemplateMatcher.cs
+++ src/FakR.Core/TemplateMatcher.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace FakR.Core
 {
@@ -10,10 +12,22 @@ namespace FakR.Core
         {
             _templateStore = templateStore;
         }
+
+        public string Match(string content)
+        {
+            return _templateStore.GetTemplate(ProduceContentHash(content));
+        }
+
+        private string ProduceContentHash(string content)
+        {
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            // This is one implementation of the abstract class SHA1.
+            return Convert.ToBase64String(sha.ComputeHash(Encoding.UTF8.GetBytes(content)));
+        }
     }
 
     public interface ITemplateStore
     {
-        
+        string GetTemplate(string contentHash);
     }
 }
--- tests/FakR.Tests/TemplateTests.cs
+++ tests/FakR.Tests/TemplateTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 using FakR.Core;
 using Moq;
 using NUnit.Framework;
@@ -14,12 +16,21 @@ namespace FakR.Tests
         }
 
         [Test]
-        public void GivenKnownTemplate_WhenMatchingStaticContent_ThenReturnsTemplate()
+        public void GivenKnownTemplate_WhenMatchingStaticContent_ThenCallsTemplateStoreWithBase64Hash()
         {
+            string content = "{ \"Message:\" \"Hello world\" }";
+
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            // This is one implementation of the abstract class SHA1.
+            string expectedHash = Convert.ToBase64String(sha.ComputeHash(Encoding.UTF8.GetBytes(content)));
+
             Mock<ITemplateStore> mockTemplateStore = new Mock<ITemplateStore>();
 
             TemplateMatcher templateMatcher = new TemplateMatcher(mockTemplateStore.Object);
 
+            templateMatcher.Match(content);
+
+            mockTemplateStore.Verify(x => x.GetTemplate(It.Is<string>(hash => hash == expectedHash)), Times.Once);
         }
     }
 }
