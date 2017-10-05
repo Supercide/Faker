84ede31cab1ceb4f85f8fac7e05256ff16b035bc
Jordan anderson <jordan_anders@live.co.uk>
2017-10-05 21:10:51 +0100

    Adds dynamic templating

--- src/FakR.Core/ResponseFactory.cs
+++ src/FakR.Core/ResponseFactory.cs
@@ -1,11 +1,36 @@
-﻿using FakR.Core;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
 
-namespace FakR.Tests {
+namespace FakR.Core {
     public class ResponseFactory
     {
-        public string Create(Template template)
+        public string Create(string request, Template template)
         {
-            return template.Response;
+            var obj = JObject.Parse(request);
+            var properties = obj.Properties().Select(property => property.Name).ToList();
+            Regex regex = new Regex(@"\{\{(\S+)\}\}");
+            var matches = regex.Matches(template.Response);
+            string response = template.Response;
+            foreach (Match match in matches)
+            {
+                var propertyName = match.Groups[1].Value;
+
+                var replacement = string.Empty;
+
+                if (properties.Contains(propertyName))
+                {
+                    var token = obj[propertyName];
+
+                    replacement = token.Value<string>();
+                }
+
+                response = response.Replace(match.Value, replacement);
+
+
+            }
+            
+            return response;
         }
     }
 }
--- tests/FakR.Tests/ResponseCreationTests.cs
+++ tests/FakR.Tests/ResponseCreationTests.cs
@@ -5,20 +5,54 @@ namespace FakR.Tests {
     public class ResponseCreationTests
     {
         [Test]
-        public void GivenTemplate_WhenCreatingResponse_ThenCreatesResponse()
+        public void GivenRequest_WhenCreatingResponseFromStaticTemplate_ThenCreatesResponse()
         {
+            var request = "{ \"a\": \"1\",\"b\": \"2\",\"c\": \"3\" }";
+
             Template template = new Template
             {
-                Request = "{ \"a\": \"1\",\"b\": \"2\",\"c\": \"3\" }",
                 Response = "{ \"a\": \"1\",\"b\": \"2\",\"c\": \"3\" }"
             };
 
             ResponseFactory factory = new ResponseFactory();
 
-            var response = factory.Create(template);
+            var response = factory.Create(request, template);
 
             Assert.That(response, Is.EqualTo(template.Response));
+        }
 
+        [Test]
+        public void GivenDynamicTemplate_WhenCreatingResponse_ThenCreatesResponse()
+        {
+            var request = "{ \"a\": \"1\",\"b\": \"2\",\"c\": \"3\" }";
+            var expected = "{ \"Message\": \"Captured : 1, 2, 3\" }";
+            Template template = new Template
+            {
+                Response = "{ \"Message\": \"Captured : {{a}}, {{b}}, {{c}}\" }"
+            };
+
+            ResponseFactory factory = new ResponseFactory();
+
+            var response = factory.Create(request, template);
+
+            Assert.That(response, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void GivenDynamicTemplate_WithRequestMissingToken_WhenCreatingResponse_ThenCreatesResponse_WithMissingTokenEmpty()
+        {
+            var request = "{ \"a\": \"1\",\"b\": \"2\",\"c\": \"3\" }";
+            var expected = "{ \"Message\": \"Captured : 1, 2, 3 \" }";
+            Template template = new Template
+            {
+                Response = "{ \"Message\": \"Captured : {{a}}, {{b}}, {{c}} {{d}}\" }"
+            };
+
+            ResponseFactory factory = new ResponseFactory();
+
+            var response = factory.Create(request, template);
+
+            Assert.That(response, Is.EqualTo(expected));
         }
     }
 }
