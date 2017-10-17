d8f2cc9377e2a897d833a18559004811fcedbb8c
Jordan anderson <jordan_anders@live.co.uk>
2017-10-17 22:08:37 +0100

    Restores template matcher tests

---
+++ tests/Faker.Tests/FakeTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Faker.Core.Extensions;
+
+namespace Faker.Tests {
+    public class FakeTemplate : ITemplate
+    {
+        public IList<string> GetProperties()
+        {
+            return Properties;
+        }
+        public string Request { get; set; }
+        public string[] Properties { get; set; }
+        public string Response { get; set; }
+    }
+}
